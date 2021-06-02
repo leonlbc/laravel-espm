66f5738bc4ada7f9b97e79a9d681f4f8246814f0
leon <cohenleonlbc@gmail.com>
2021-06-02 03:06:23 -0300

    ok

---
+++ resources/views/cebolokor.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Grupo Dois</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding-top: 40px;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Nunito', sans-serif;
+            }
+
+            .container {
+                text-align: center;
+                padding: 10px;
+                display: grid;
+                vertical-align: middle;
+            }
+            .title {
+                font-size: 96px;
+                text-align: center;
+            }
+            .text {
+                font-size: 30px;
+            }
+            .dia {
+                font-size: 25px;
+            }
+            .foto_perfeita_do_melhor_professor {
+              display: block;
+              margin-left: auto;
+              margin-right: auto;
+              width: 25%;
+              height: 25%;
+            }
+        </style>
+    </head>
+    <body>
+      <div class="title">CEBOLOKOR</div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cebolokor', function () {
+    return view('cebolokor');
+});
